69835558ecc378bb9112feebe266e3ab9110872c
wtikim <wtikim@dankook.ac.kr>
2025-12-01 15:00:45 +0000

    committed

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
@@ -10,10 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB 연결
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.error(err));
 
